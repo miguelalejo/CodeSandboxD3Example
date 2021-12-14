345b250cd3743cea93f9e1531e6a0bedeca1f394
miguelalejo <mikasomk34#@hotmail.com>
2021-12-14 15:20:25 -0500

    se aggrega cambios

--- script-radar.js
+++ script-radar.js
@@ -386,3 +386,59 @@ d3.json( "https://raw.githubusercontent.com/miguelalejo/CodeSandboxD3Example/mai
 
 
 });
+
+d3.json( "https://raw.githubusercontent.com/miguelalejo/CodeSandboxD3Example/main/data/radar-size-company.json", function(data) {
+  data = data.map(function(d) { return d.values })
+  RadarChartC.draw("#chart-4", data, config);
+  var colorscale = d3.scale.category10();
+  var w = 500,
+    h = 500;
+    
+  var svg = d3.select('#body')
+    .selectAll('svg')
+    .append('svg')
+    .attr("width", w+300)
+    .attr("height", h)
+  
+  //Create the title for the legend
+  var text = svg.append("text")
+    .attr("class", "title")
+    .attr('transform', 'translate(90,0)') 
+    .attr("x", w - 70)
+    .attr("y", 10)
+    .attr("font-size", "12px")
+    .attr("fill", "#404040")
+    .text("What % of owners use a specific service in a week");
+      
+  //Initiate Legend	
+  var legend = svg.append("g")
+    .attr("class", "legend")
+    .attr("height", 100)
+    .attr("width", 200)
+    .attr('transform', 'translate(90,20)') 
+    ;
+    //Create colour squares
+    legend.selectAll('rect')
+      .data(LegendOptions)
+      .enter()
+      .append("rect")
+      .attr("x", w - 65)
+      .attr("y", function(d, i){ return i * 20;})
+      .attr("width", 10)
+      .attr("height", 10)
+      .style("fill", function(d, i){ return colorscale(i);})
+      ;
+    //Create text next to squares
+    legend.selectAll('text')
+      .data(LegendOptions)
+      .enter()
+      .append("text")
+      .attr("x", w - 52)
+      .attr("y", function(d, i){ return i * 20 + 9;})
+      .attr("font-size", "11px")
+      .attr("fill", "#737373")
+      .text(function(d) { return d; })
+      ;	
+
+
+});
